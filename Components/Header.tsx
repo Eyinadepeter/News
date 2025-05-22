9fa3fa070f1d105ca7d7a0a936cc97951ad736b8
Eyinade Peter <itemize6@gmail.com>
2025-05-22 12:41:59 +0100

    header

--- Components/Header.tsx
+++ Components/Header.tsx
@@ -1,15 +1,44 @@
-import Link from 'next/link'
-import React from 'react'
-
+import Link from "next/link";
+import React from "react";
 
 function header() {
   return (
     <div>
       <Link href="#">Channel 21</Link>
-      <div className='flex item-center w-1/3 text-lg'>
-      <input type="text" className='bg-transparent border-b w-full border-white outline-none ' /></div>
+      <div className="flex item-center w-1/3 text-lg">
+        <input
+          type="text"
+          className="bg-transparent border-b w-full border-white outline-none "
+        />
+      </div>
+      <div className="bg-white flex items-center justify-center text-black h-16">
+        <Link
+          href="#"
+          className="mr-10 text-xl mt-5 px-5 border-b-2 border-transparent hover:border-black"
+        >
+          Home
+        </Link>
+        <Link
+          href="#"
+          className="mr-10 text-xl mt-5 px-5 border-b-2 border-transparent hover:border-black"
+        >
+          Politics
+        </Link>{" "}
+        <Link
+          href="#"
+          className="mr-10 text-xl mt-5 px-5 border-b-2 border-transparent hover:border-black"
+        >
+          Bussiness
+        </Link>
+        <Link
+          href="#"
+          className="mr-10 text-xl mt-5 px-5 border-b-2 border-transparent hover:border-black"
+        >
+          Sport
+        </Link>
+      </div>
     </div>
   );
 }
 
-export default header
+export default header;
