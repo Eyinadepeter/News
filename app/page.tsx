4be0a7cda641b78c4053bef6a77b5bf7427bed70
Eyinadepeter <116883885+Eyinadepeter@users.noreply.github.com>
2025-11-17 14:14:49 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -7,6 +7,7 @@ export default function Home() {
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <Header/>
       <h1>Hi from Home</h1>
+
       <Footer/>
     </div>
   );
