510303c4deb29c280967128d04c17fb89f26027c
Eyinadepeter <116883885+Eyinadepeter@users.noreply.github.com>
2025-11-17 14:16:11 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
       <h1>Hi from Home</h1>
       <h1>Hi from Home</h1>
       <h1>Hi from Home</h1>
-
+      <h1>Hi from Home</h1>
       <Footer/>
     </div>
   );
